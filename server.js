55f31f2eb78e9693eb29ee8bb2895b072eb4a799
Cole Joko <joko.cole.cj@gmail.com>
2020-08-29 14:32:04 -0600

    debug

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
-const express = require('express');
+const express = require("express");
 const api = require("./Routes/api.js");
 const html = require("./Routes/html.js");
+
+var gradient = require('gradient-string');
+
 const app = express();
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
-
-app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-app.use(express.static('./public'));
-
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use("/api", api);
-app.use("/html", html);
+app.use("/", html);
 
-app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
+app.listen(PORT, () => 
+  console.log(gradient.cristal(`App listening on: http://localhost:${PORT}`))
+);
